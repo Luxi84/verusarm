760648f84acfd4e886d4585f9565d34d0e075a29
Luxi84 <32354483+Luxi84@users.noreply.github.com>
2024-09-14 19:14:15 +0200

    Create install-opt.sh

---
+++ install-opt.sh
@@ -0,0 +1,4 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential libomp5 git -y
+cd /usr/src
+sudo git clone https://github.com/Oink70/CCminer-ARM-optimized
+cd /usr/src/CCminer-ARM-optimized/
