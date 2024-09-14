1a2c6ae9ffa16091af27a5a009f5be1d4c5c8360
Luxi84 <32354483+Luxi84@users.noreply.github.com>
2024-09-14 19:16:28 +0200

    Update install-opt.sh

--- install-opt.sh
+++ install-opt.sh
@@ -2,3 +2,4 @@ sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake aut
 cd /usr/src
 sudo git clone https://github.com/Oink70/CCminer-ARM-optimized
 cd /usr/src/CCminer-ARM-optimized/
+chmod x start.sh
